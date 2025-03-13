0131d9f0e5475f2022c8d0e7bfb9af0a22cb710f
alexanderhlee <alexanderhlee@gmail.com>
2025-03-13 19:18:48 -0400

    Initial commit: Create SortingVisualizerApp Xcode project

---
+++ SortingVisualizerApp/SortingVisualizerApp/SortingVisualizerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingVisualizerAppApp.swift
+//  SortingVisualizerApp
+//
+//  Created by Alexander Lee on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SortingVisualizerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SortingVisualizerApp/SortingVisualizerAppTests/SortingVisualizerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SortingVisualizerAppTests.swift
+//  SortingVisualizerAppTests
+//
+//  Created by Alexander Lee on 3/13/25.
+//
+
+import Testing
+@testable import SortingVisualizerApp
+
+struct SortingVisualizerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
