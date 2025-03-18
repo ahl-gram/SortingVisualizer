713a31cdd4644c8836b5784a25f0898ce5a5fdf9
alexanderhlee <github@route12b.net>
2025-03-17 22:06:00 -0400

    Remove extra

--- SortingVisualizerApp/SortingVisualizerApp/SortingAlgorithms.swift
+++ SortingVisualizerApp/SortingVisualizerApp/SortingAlgorithms.swift
@@ -49,27 +49,6 @@ enum SortingAlgorithmType: String, CaseIterable, Identifiable {
 /// Pure sorting algorithms without visualization or audio logic
 enum SortingAlgorithms {
     
-    // MARK: - Sorting Step Types
-    
-    /// Represents a step in the sorting process
-    enum SortingStep<T> {
-        /// Comparing two elements
-        case compare(Int, Int)
-        /// Swapping two elements
-        case swap(Int, Int)
-        /// Merging operation (specific to merge sort)
-        case merge(Int, T)
-        /// Marking an element as sorted
-        case markSorted(Int)
-        /// Bucket operation for non-comparison sorts (like radix)
-        case bucket(Int, Int)
-        /// Algorithm completed
-        case completed
-    }
-    
-    /// Callback signature for reporting sorting steps
-    typealias StepCallback<T> = (SortingStep<T>, [T]) async -> Bool
-    
     // MARK: - Bubble Sort
     static func bubbleSort<T: Comparable>(
         array: [T],
